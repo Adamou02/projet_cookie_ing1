5634501e069201fd2b16d80c5388456d7a06eb5e
LegJo <106883191+LegJo@users.noreply.github.com>
2022-11-29 21:25:37 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,27 +8,27 @@
     {
         EditTerminal();
         InitRNG();
+        
         float float_diffRate = ChooseDifficulty();
         int int_mapSize = ChooseMapSize();
+        
         PlayerInfo s_playerInfo = SetupPlayer(); 
         InitEnergy(&s_playerInfo, int_mapSize);
-        List* p_listpath = InitList();
         
-
+        List* p_listpath = InitList(s_playerInfo.coordonnees);
+       
         int** matrice_Map = InitMap(int_mapSize, 
                                     float_diffRate,
                                     &s_playerInfo);
         int*** matrice_Distance = InitDistance(int_mapSize);
         
-        int bool_victory = 0;
-
+        
         DisplayMap(matrice_Map, int_mapSize);
         //DebugDisplayMatriceDistance(int_mapSize, matrice_Distance);
         printf("Votre energie : %d\n",s_playerInfo.energy);
         ShowKeyAvailable(&s_playerInfo, matrice_Distance);
 
-        
-        bool_victory = Game(int_mapSize, matrice_Map, matrice_Distance,&s_playerInfo, p_listpath);
+        int bool_victory = Game(int_mapSize, matrice_Map, matrice_Distance,&s_playerInfo, p_listpath);
         // DebugDisplayMap(matrice_Map, int_mapSize);  
 
 
