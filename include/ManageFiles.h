497fc46a3be80215795d19ff1b5c0ab4b6c58af0
Fab780 <104725129+Fab780@users.noreply.github.com>
2022-11-29 16:22:14 +0100

    Update ManageFiles.h

--- include/ManageFiles.h
+++ include/ManageFiles.h
@@ -9,5 +9,21 @@
     void createFile(char char_NameFile[]);
    
     int SupprFile(char char_NameFile[]);
+
+    char* AllocMapString(int int_mapSize);
+
+    char* AllocDistanceString(int int_mapSize);
+
+    char* MatriceMapToString(int** matrice_Map, int int_mapSize, char* MapString);
+
+    char* MatriceDistanceToString(int*** matrice_Distance, int int_mapSize, char* DistanceString);
     
+    int** StringToMatriceMap(char* MapString, int int_mapSize, int** matrice_Map);
+
+    int*** StringToMatriceDistance(char* DistanceString, int int_mapSize, int*** matrice_Distance);
+
+    void StockCurrentTurn(char* MapString, char* DistanceString, PlayerInfo * s_playerInfo_player);
+
+    void GetAllInfo(char* MapString, char* DistanceString, PlayerInfo * s_playerInfo_player, int int_mapSize);
+
 #endif
