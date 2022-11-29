c2046077a4937ed045939252a25b43a440ef59a6
Clément <82080806+clementcst@users.noreply.github.com>
2022-11-29 11:21:24 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,57 +1,40 @@
-#include "Miscellanous.h"
-#include "Display.h"
-#include "Movements.h"
-#include "InitGame.h"
-#include "ManageFiles.h"
+    #include "Miscellanous.h"
+    #include "Display.h"
+    #include "Movements.h"
+    #include "InitGame.h"
+    #include "ManageFiles.h"
 
-int main(void)
-{
-    EditTerminal();
-    InitRNG();
-    float float_diffRate = ChooseDifficulty();
-    int int_mapSize = ChooseMapSize();
-    PlayerInfo s_playerInfo = SetupPlayer(); 
-    InitEnergy(&s_playerInfo, int_mapSize);
-    List* p_listpath = InitList();
+    int main(void)
+    {
+        EditTerminal();
+        InitRNG();
+        float float_diffRate = ChooseDifficulty();
+        int int_mapSize = ChooseMapSize();
+        PlayerInfo s_playerInfo = SetupPlayer(); 
+        InitEnergy(&s_playerInfo, int_mapSize);
+        List* p_listpath = InitList();
+        
 
-    int bool_victory = 0;
-    
-    
+        int** matrice_Map = InitMap(int_mapSize, 
+                                    float_diffRate,
+                                    &s_playerInfo);
+        int*** matrice_Distance = InitDistance(int_mapSize);
+        
+        int bool_victory = 0;
 
-    int** matrice_Map = InitMap(int_mapSize, 
-                                float_diffRate,
-                                &s_playerInfo);
-    int*** matrice_Distance = InitDistance(int_mapSize);
-    
-    
-
-    DisplayMap(matrice_Map, int_mapSize);
-    //DebugDisplayMatriceDistance(int_mapSize, matrice_Distance);
-    printf("Votre energie : %d\n",s_playerInfo.energy);
-    ShowKeyAvailable(&s_playerInfo, matrice_Distance);
-
-    
-
-
-    
-    int key_pressed;
-    int int_wanted_x;
-    int int_wanted_y;
-    while(bool_victory == 0 && s_playerInfo.energy > 0){
-        key_pressed = ListenKeyboard();
-        ChangePosition(key_pressed, &int_wanted_x, &int_wanted_y, &s_playerInfo); 
-        matrice_Map = AfterMovement(matrice_Map, int_wanted_x, int_wanted_y, &s_playerInfo, int_mapSize, &bool_victory);
-        printf("\n"); 
-        DisplayMap(matrice_Map, int_mapSize); 
+        DisplayMap(matrice_Map, int_mapSize);
+        //DebugDisplayMatriceDistance(int_mapSize, matrice_Distance);
         printf("Votre energie : %d\n",s_playerInfo.energy);
         ShowKeyAvailable(&s_playerInfo, matrice_Distance);
+
+        
+        bool_victory = game(int_mapSize, matrice_Map, matrice_Distance,&s_playerInfo);
+        // DebugDisplayMap(matrice_Map, int_mapSize);  
+
+
+        PrintList(p_listpath);
+        FreeList(p_listpath);
+        UnallocMatriceMap(matrice_Map, int_mapSize);
+        UnallocMatriceDistance(matrice_Distance, int_mapSize);
+        return 0;
     }
-    // DebugDisplayMap(matrice_Map, int_mapSize);  
-
-
-    PrintList(p_listpath);
-    FreeList(p_listpath);
-    UnallocMatriceMap(matrice_Map, int_mapSize);
-    UnallocMatriceDistance(matrice_Distance, int_mapSize);
-    return 0;
-}
