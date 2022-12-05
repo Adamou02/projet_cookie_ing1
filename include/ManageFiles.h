7fd7bda745e5bae9be89d27c8992ff208649d652
Fab780 <104725129+Fab780@users.noreply.github.com>
2022-12-05 15:49:45 +0100

    Update ManageFiles.h

--- include/ManageFiles.h
+++ include/ManageFiles.h
@@ -11,21 +11,25 @@
    
     int SupprFile(char char_NameFile[]);
 
-    void GetPlayerInfo(int int_mapSize, PlayerInfo * s_playerInfo_player);
+    int SavedTurnsCount(int int_mapSize);
 
-    char* GetMapString(char* MapString, int int_mapSize);
+    void GetPlayerInfo(int int_mapSize, PlayerInfo * s_playerInfo_player, int num_turn);
 
-    char* GetDistanceString(char* DistanceString, int int_mapSize);
+    char* GetMapString(char* MapString, int int_mapSize, int num_turn);
+
+    char* GetDistanceString(char* DistanceString, int int_mapSize, int num_turn);
 
     char* AllocMapString(int int_mapSize);
 
     char* AllocDistanceString(int int_mapSize);
 
+    char* AllocListString(int int_mapSize);
+
     char* MatriceMapToString(int** matrice_Map, int int_mapSize, char* MapString);
 
-    char* MatriceMapToString2(int** matrice_Map, int int_mapSize, char* MapString);
-
     char* MatriceDistanceToString(int*** matrice_Distance, int int_mapSize, char* DistanceString);
+
+    char* ListToString(List* p_list, char* ListString);
     
     int** StringToMatriceMap(char* MapString, int int_mapSize, int** matrice_Map);
 
@@ -33,12 +37,16 @@
 
     char* SaveMap(int** matrice_Map, int int_mapSize);
 
-    int** RestoreMap(int** matrice_Map, int int_mapSize);
+    int** RestoreMap(int** matrice_Map, int int_mapSize, int num_turn);
 
-    int*** RestoreDistance(int*** matrice_Distance, int int_mapSize);
+    char* SaveDistance(int*** matrice_Distance, int int_mapSize);
 
-    void GetAllInfo(char* MapString, char* DistanceString, PlayerInfo * s_playerInfo_player, int int_mapSize);
+    int*** RestoreDistance(int*** matrice_Distance, int int_mapSize, int num_turn);
 
-    void StockCurrentTurn(int** matrice_Map, int*** matrice_Distance, int int_mapSize, PlayerInfo * s_playerInfo_player);
+    char* SaveList(List* p_list, int int_mapSize);
+
+    void StockCurrentTurn(int** matrice_Map, int*** matrice_Distance, List* p_list, int int_mapSize, PlayerInfo * s_playerInfo_player);
+
+    void RestoreTurn(int num_turn, int*** matrice_Map, int**** matrice_Distance, PlayerInfo* s_playerInfo, int int_mapSize);
 
 #endif
