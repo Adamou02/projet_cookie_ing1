4ea38d6e088a4ab7e953269e972e9baebd232177
LegJo <106883191+LegJo@users.noreply.github.com>
2022-12-17 15:59:58 +0100

    Update Display.h

--- include/Display.h
+++ include/Display.h
@@ -6,37 +6,27 @@
     #include "Constant.h"
     #include "Miscellanous.h"
     
-    /// @brief Modifie le terminal pour l'adapter au jeu 
     void EditTerminal(); //Agrandi le terminal ou le maximize si wmctrl est installer
 
-    /// @brief Menu lorsqu'on lance le programme, permet de lancer une nouvelle game, charger la précedente, ou voir l'historique.
+    
     int MenuStartGame();
 
-    /// @brief Menu pour choisir la difficulté 
     int MenuDifficulty();
-
-    /// @brief Menu pour choisir la taille de la carte
+    
     int MenuMapSize();
 
-    /// @brief Menu lors de la fermeture de la partie
     int MenuSave();
 
-    /// @brief Fonction d'affichage de la Map dans le terminal
     void DisplayMap(int** matrice_Map, int int_mapSize);
 
-    /// @brief affiche la map avec le chemin suivis par une liste passer en parametre
     void DisplayPathInMap(int** matric_Map, int int_mapSize, List* p_list);
 
-    /// @brief affiche la map avec le chemin suivis par une liste passer en parametre avec des fleches correspondant a la direction
     void DisplayPathInMapArrow(int** matric_Map, int int_mapSize, List* p_list);
 
-    /// @brief Appelle l'affichage victoire ou defaite selon la valeur de bool_victory
     void DisplayEndGame(int bool_victory, PlayerInfo *p_playerInfo);
 
-    /// @brief Affiche l'écran de victoire
     void DisplayVictory(PlayerInfo *p_playerInfo);
     
-    /// @brief Affiche l'écran de défaite
     void DisplayLoose();
 
     /////////////////////////////////////////////////////////////////////////////
